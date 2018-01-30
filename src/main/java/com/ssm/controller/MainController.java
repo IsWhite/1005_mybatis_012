24fe88a49f730b5b74bb98081eca325de5939987
IsWhite <2240641509@qq.com>
2018-01-30 09:26:28 +0800

    ssm 增删改查

---
+++ src/main/java/com/ssm/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dllo on 18/1/29.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = {"/",""})
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/ssm/test/StudentTest.java
@@ -0,0 +1,60 @@
+package com.ssm.test;
+
+import com.ssm.domain.Student;
+import com.ssm.mapper.StudentDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 18/1/29.
+ */
+public class StudentTest {
+
+    private ApplicationContext context;
+    private StudentDao studentDao;
+
+    @Before
+    public void init(){
+        context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
+        studentDao = context.getBean(StudentDao.class);
+    }
+
+    @Test
+    public  void testSelectAll(){
+        List<Student> students =studentDao.selectAll();
+        System.out.println(students);
+    }
+
+    @Test
+    public void selectById(){
+        Student student =studentDao.selectById(17);
+        System.out.println(student);
+    }
+
+    @Test
+    public void insertStudent(){
+        Student student =new Student("张三","123");
+        int key =studentDao.insertStudent(student);
+        System.out.println("插入的个数为"+key);
+        System.out.println("主键id为"+student);//增加了useGeneratedKeys属性
+    }
+
+    @Test
+    public void deleteById(){
+        int count =studentDao.deleteById(19);
+        System.out.println(count);
+    }
+    @Test
+    public void updateById(){
+        Student student =studentDao.selectById(17);
+        student.setName("王");
+        student.setPassword("111");
+
+        int count =studentDao.updateById(student);
+        System.out.println(count);
+    }
+}
